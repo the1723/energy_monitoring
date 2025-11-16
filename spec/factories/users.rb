36270f661e6b9f61b5c33bf2b027c0f278991c7b
the1723 <hello@paveldavydov.com>
2025-11-16 16:29:21 +0100

    feat: added users

---
+++ spec/factories/users.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           not null
+#  encrypted_password  :string           not null
+#  name                :string           not null
+#  remember_created_at :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+FactoryBot.define do
+  factory :user do
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           not null
+#  encrypted_password  :string           not null
+#  name                :string           not null
+#  remember_created_at :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
